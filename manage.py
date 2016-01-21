b29a70d702c6b9faae9cfa59cbfffeed08cb96ca
Coderhypo <i@ihypo.net>
2016-01-21 13:15:56 +0800

    update manage

--- manage.py
+++ manage.py
@@ -2,10 +2,20 @@
 # coding=utf-8
 from app import app
 from flask.ext.script import Manager, Server
+from config import OJS
+from app.crawl import OJInitCrawler
 
 manager = Manager(app)
 
-manager.add_command("start", Server(host="0.0.0.0", port=5000, use_debugger=True))
+manager.add_command("debug", Server(host="0.0.0.0", port=5000, use_debugger=True))
+manager.add_command("start", Server(host="0.0.0.0", port=5000))
+
+@manager.command
+def init():
+    for oj_code in OJS:
+        init_crawler = OJInitCrawler()
+        init_crawler.crawl(oj_code)
+
 
 if __name__ == '__main__':
     manager.run()
