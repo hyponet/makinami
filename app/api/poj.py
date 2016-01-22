c68a09b3c072e0d797e931b650e6aaa5b90d289e
Coderhypo <i@ihypo.net>
2016-01-22 12:43:43 +0800

    update poj

--- app/api/poj.py
+++ app/api/poj.py
@@ -4,9 +4,11 @@
 # 提供POJ相关API的实现
 
 import pymongo
+from flask import request
 from flask.ext import restful
 
 from app import api
+from app.crawl import ProblemCrawler, StatusCrawler, CodeSubmitCrawler, AccountCrawler
 import config
 
 class POJProblem(restful.Resource):
@@ -16,7 +18,13 @@ class POJProblem(restful.Resource):
         db = client[config.MONGO_DATABASE]
         
         problem_info = db['problems'].find_one({'oj': 'poj', 'problem_id': str(problem_id)})
+        if problem_info is None:
+            get_problem = ProblemCrawler()
+            get_problem.crawl('poj', str(problem_id))
+
+            problem_info = db['problems'].find_one({'oj': 'poj', 'problem_id': str(problem_id)})
         client.close()
+
         if problem_info is None:
             return{
                 'status': 404,
@@ -26,7 +34,7 @@ class POJProblem(restful.Resource):
         return {
             'status': 200,
             'oj': 'poj',
-            'problem_id': problem_id,
+            'problem_id': str(problem_id),
             'title': problem_info['title'] if 'title' in problem_info else '',
             'description': problem_info['description'] if 'description' in problem_info else '',
             'input': problem_info['input'] if 'input' in problem_info else '',
@@ -37,12 +45,59 @@ class POJProblem(restful.Resource):
             'memory_limit': problem_info['memory_limit'] if 'memory_limit' in problem_info else '65535K'
         }
 
+    def post(self, problem_id):
+        code_submit = CodeSubmitCrawler()
+        code_submit.crawl(
+            oj='poj',
+            problem_id=str(problem_id),
+            language=request.json['language'],
+            code=request.json['code'],
+            username=request.json['username'],
+            password=request.json['password']
+            )
+
+        client = pymongo.MongoClient(config.MONGO_URI)
+        db = client[config.MONGO_DATABASE]
+        
+        problem_info = db['status'].find_one({'oj': 'poj', 'problem_id': str(problem_id)})
+        client.close()
+
+        if problem_info is None:
+            return {
+                'status': 500,
+                'message': 'failed to submit'
+            }
 
 class POJStatus(restful.Resource):
 
     def get(self, run_id):
-        pass
+        client = pymongo.MongoClient(config.MONGO_URI)
+        db = client[config.MONGO_DATABASE]
+        
+        problem_info = db['status'].find_one({'oj': 'poj', 'run_id': str(run_id)})
+        if problem_info is None:
+            get_status = StatusCrawler()
+            get_status.crawl('poj', str(run_id))
+            problem_info = db['status'].find_one({'oj': 'poj', 'run_id': str(run_id)})
+        client.close()
+    
+        if problem_info is None:
+            return {
+                'status': 404,
+                'message': 'not found'
+            }
 
+        return {
+            'oj': 'poj',
+            'run_id': str(run_id),
+            'problem_id': problem_info['problem_id'],
+            'result': problem_info['result'],
+            'memory': problem_info['memory'] if 'memory' in problem_info else '',
+            'time': problem_info['time'] if 'time' in problem_info else '',
+            'language': problem_info['language'],
+            'code_length': problem_info['code_length'],
+            'submit_time': problem_info['submit_time']
+        }
 
 class POJUsers(restful.Resource):
 
